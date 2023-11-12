559dc1525f81bc41d7bf76007fd4716a1df476d8
Willy Xamountry <Willy.Xamountry@googlemail.com>
2023-11-12 21:02:11 +0100

    Updated Setup

--- RPI/Deployment/installPythonLibraries.sh
+++ RPI/Deployment/installPythonLibraries.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+#install matplotlib
+sudo apt install python3-matplotlib
+
+#Setup a virtual python environment
+cd ~
+pip3 install --upgrade adafruit-python-shell
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py
+
+source env/bin/activate
+
 #Python Libraries
 pip3 install adafruit-circuitpython-amg88xx
+pip install colour
+
 #install Pi-Apps for OBS
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
