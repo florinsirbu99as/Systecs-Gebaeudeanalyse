c6aa1027f56abe7f458856fe972cb3b75bbad0db
Willy Xamountry <willy.xamountry@googlemail.com>
2023-11-15 23:31:03 +0100

    Correct implementation of Baudrate

--- RPI/Deployment/Scripts/writeBaudrate.py
+++ RPI/Deployment/Scripts/writeBaudrate.py
@@ -1,17 +1,24 @@
 import os
 
+#default path for this file on Raspbian
 dir = "/boot/config.txt"
 status = ""
+
+#Check if File exists
 if os.path.exists(dir):
     status = "File exists."
-    with open(dir) as file:
-        for lineIndex in range(file.readlines):
-            line = file.readlines[lineIndex]
+    #first read(r) then update(+) File
+    with open(dir, mode="r+") as file:
+        lines = file.readlines()
+        for lineIndex in range(len(lines)):
+            line = lines[lineIndex]
             if line == "dtparam=i2c_arm=on":
                 status = "Line exists."
-                file.readlines[lineIndex] = "dtparam=i2c_arm=on, i2c_arm_baudrate=400000"
-                status = "Line written."
+                lines[lineIndex] = "dtparam=i2c_arm=on, i2c_arm_baudrate=400000"
                 break
+        if status == "Line exists.":
+            file.writelines(lines)
+            status = "Baudrate written."
 else:
     status = "File does not exist."
 
