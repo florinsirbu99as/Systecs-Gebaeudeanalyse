3e267062710118dc3382d28a52c88de3387f5595
Willy Xamountry <willy.xamountry@googlemail.com>
2023-11-15 23:40:02 +0100

    BugFix string compare

--- RPI/Deployment/Scripts/writeBaudrate.py
+++ RPI/Deployment/Scripts/writeBaudrate.py
@@ -2,6 +2,7 @@ import os
 
 #default path for this file on Raspbian
 dir = "/boot/config.txt"
+dir = "C:/temp/test.txt"
 status = ""
 
 #Check if File exists
@@ -9,10 +10,11 @@ if os.path.exists(dir):
     status = "File exists."
     #first read(r) then update(+) File
     with open(dir, mode="r+") as file:
+        status = "File opened."
         lines = file.readlines()
         for lineIndex in range(len(lines)):
             line = lines[lineIndex]
-            if line == "dtparam=i2c_arm=on":
+            if line.find("dtparam=i2c_arm=on")>-1:
                 status = "Line exists."
                 lines[lineIndex] = "dtparam=i2c_arm=on, i2c_arm_baudrate=400000"
                 break
