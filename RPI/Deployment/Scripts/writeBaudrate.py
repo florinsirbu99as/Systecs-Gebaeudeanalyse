a48dec02d5211e1b36d040b05f2fd114c323f79e
Willy Xamountry <willy.xamountry@googlemail.com>
2023-11-15 23:45:50 +0100

    BugFix wrong File open mode

--- RPI/Deployment/Scripts/writeBaudrate.py
+++ RPI/Deployment/Scripts/writeBaudrate.py
@@ -1,24 +1,26 @@
 import os
 
-#default path for this file on Raspbian
+# default path for this file on Raspbian
 dir = "/boot/config.txt"
 dir = "C:/temp/test.txt"
 status = ""
 
-#Check if File exists
+# Check if File exists
 if os.path.exists(dir):
     status = "File exists."
-    #first read(r) then update(+) File
+    # first read(r) then update(+) File
     with open(dir, mode="r+") as file:
         status = "File opened."
         lines = file.readlines()
         for lineIndex in range(len(lines)):
             line = lines[lineIndex]
-            if line.find("dtparam=i2c_arm=on")>-1:
+            if line.find("dtparam=i2c_arm=on") > -1:
                 status = "Line exists."
                 lines[lineIndex] = "dtparam=i2c_arm=on, i2c_arm_baudrate=400000"
                 break
         if status == "Line exists.":
+            file.seek(0)
+            file.truncate()
             file.writelines(lines)
             status = "Baudrate written."
 else:
