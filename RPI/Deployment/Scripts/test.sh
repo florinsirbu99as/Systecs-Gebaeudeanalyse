78ac7b48ef425600d2592b94ffd58356c934cff4
Willy Xamountry <willy.xamountry@googlemail.com>
2023-11-16 03:18:06 +0100

    added gitignore; Install Skript should work now as intended

---
+++ RPI/Deployment/Scripts/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#install matplotlib
+sudo apt update
+sudo apt install -y python3-matplotlib &
+sudo apt install -y python3-colour &
+sudo apt install -y python3-numpy &
+sudo apt install -y python3-scipy &
+sudo apt install -y python3.11-venv &
--- RPI/Deployment/installPythonLibraries.sh
+++ RPI/Deployment/installPythonLibraries.sh
@@ -4,19 +4,28 @@
 sudo python ~/VSC/Systecs-Gebaeudeanalyse/RPI/Deployment/Scripts/writeBaudrate.py
 
 #install matplotlib
-sudo apt install python3-matplotlib
+sudo apt update
+sudo apt install -y python3-matplotlib
+sudo apt install -y python3-colour
+sudo apt install -y python3-numpy
+sudo apt install -y python3-scipy
+sudo apt install -y python3.11-venv
 
 #Setup a virtual python environment
 cd ~
-pip3 install --upgrade adafruit-python-shell
-wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
-sudo -E env PATH=$PATH python3 raspi-blinka.py
-
+sudo python -m venv env --system-site-packages
 source env/bin/activate
 
-#Python Libraries
+cd ~
+pip3 install --upgrade adafruit-python-shell
 pip3 install adafruit-circuitpython-amg88xx
-pip install colour
+wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+sudo -E env PATH=$PATH python3 raspi-blinka.py
+deactivate
+#source env/bin/activate
+
+#Python Libraries
+#pip3 install adafruit-circuitpython-amg88xx
 
 #install Pi-Apps for OBS
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
