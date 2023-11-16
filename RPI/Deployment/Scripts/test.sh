3e3514d706263d5ef47bcf792e23f87497ac5b9e
Willy Xamountry <willy.xamountry@googlemail.com>
2023-11-16 03:19:53 +0100

    Removed Parallel Installer as it's not working; Testing .gitignore

--- RPI/Deployment/Scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#install matplotlib
-sudo apt update
-sudo apt install -y python3-matplotlib &
-sudo apt install -y python3-colour &
-sudo apt install -y python3-numpy &
-sudo apt install -y python3-scipy &
-sudo apt install -y python3.11-venv &
